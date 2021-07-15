4c0fa434fc8b3a7bddb6b9e62173c07c22ca5bcd
angelopoole <angelo.m.poole@gmail.com>
2021-07-15 16:28:49 -0400

    MaxChar Complete

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// ? first solution, frequency counter. runs through the values and creates an object that mapps each string and the ammount of times it's created to an object. this object then can be run through and the largest key/value pair can then be assigned to the largest value variable.
+function maxChar(str) {
+  let frequencyCounter = {};
+  let largestValue;
+  let localValue = 0;
+  for (let char of str) {
+    if (!frequencyCounter[char]) {
+      frequencyCounter[char] = 1;
+    } else {
+      frequencyCounter[char]++;
+    }
+  }
+
+  for (let value in frequencyCounter) {
+    if (frequencyCounter[value] > localValue) {
+      localValue = frequencyCounter[value];
+      largestValue = value;
+    }
+  }
+
+  return largestValue;
+}
+
+maxChar('iebvisdjfnaslidjfb');
 
 module.exports = maxChar;
