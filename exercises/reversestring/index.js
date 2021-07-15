e8111cb9b3660bb967ff395518584002846aec4b
angelopoole <angelo.m.poole@gmail.com>
2021-07-15 14:01:16 -0400

    reverseString completed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,42 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// ? This solution is the easy cheat solution.
+
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// ? this solution is the iterator solution
+// function reverse(str) {
+//   let newString = [];
+//   for (let letter of str) {
+//     newString.unshift(letter);
+//   }
+
+//   return newString.join('');
+// }
+
+// ? solution uses iterator and addition to add each letter to the beginning of the new string.
+// function reverse(str) {
+//   let reversed = '';
+//   for (let letter of str) {
+//     reversed = letter + reversed;
+//   }
+//   return reversed;
+// }
+
+function reverse(str) {
+  // reduce will return one value
+  // reversedString(the first argument(the accumulator)) will be what is outputted
+  // current letter is the currentValue, and will be what we add onto the reversedString value
+
+  return str.split('').reduce((reversedString, currentLetter) => {
+    return currentLetter + reversedString;
+  });
+}
+
+reverse('asdf');
+// this is to use debugger inside of vscode, or node inspect.
 
 module.exports = reverse;
