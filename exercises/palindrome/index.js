0424d45d5587a9ad08389422d03924a500ed7cd6
angelopoole <angelo.m.poole@gmail.com>
2021-07-15 16:29:11 -0400

    Palindrome complete

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,22 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// ? this soluton does 2x more work because it cross checks the array. it checks till the middle of the array, then it continues past that. This causes 2x more work since the array is already confirmed to be true or false in terms of being a palindrome.
+function palindrome(str) {
+  return str.split('').every((val, index) => {
+    return val === str[str.length - index - 1];
+  });
+}
+palindrome('acebeca');
+
+// ? first solution before watching, uses easy reverse from reverseString challenge and compares it to string to see if they're palindrome's of eachother.
+// function palindrome(str) {
+//   let reversedString = str.split('').reverse().join('');
+//   if (str === reversedString) {
+//     return true;
+//   }
+//   return false;
+// can also return str === reversedString
+// }
 
 module.exports = palindrome;
