85abdd87192e9101d7933a03b1752bb3a2f17963
angelopoole <angelo.m.poole@gmail.com>
2021-07-15 16:28:29 -0400

    reverseInt complete

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,30 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  // let intParsed = parseInt(n.toString().split('').reverse().join(''));
+  // return intParsed * Math.sign(n);
+  // * or
+  return parseInt(n.toString().split('').reverse().join('')) * Math.sign(n);
+}
+
+// ! end personal solutions
+// ? this solution first parses the int, then checks to see if the original numbe was negative. if so, it subtracts the parsed number from 0 and returns that, else return parsed number
+// function reverseInt(n) {
+//   let intParsed = parseInt(n.toString().split('').reverse().join(''));
+//   if (n < 0) {
+//     return 0 - intParsed;
+//   }
+//   return intParsed;
+// }
+
+// ? first solution, weird way of solving it, manual check for negative number
+// function reverseInt(n) {
+//   let nArr = n.toString().split('');
+//   if (nArr[0] === '-') {
+//     return parseInt('-' + nArr.reverse().join(''));
+//   }
+//   return parseInt(nArr.reverse().join(''));
+// }
 
 module.exports = reverseInt;
